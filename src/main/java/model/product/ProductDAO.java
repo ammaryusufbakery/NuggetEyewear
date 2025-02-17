f662bf810d422f48c28e2a12d846a7ea333b987e
ammaryusufbakery <ammaryusufbakery@gmail.com>
2025-02-17 15:39:24 +0800

    Update ProductDAO.java

--- src/main/java/model/product/ProductDAO.java
+++ src/main/java/model/product/ProductDAO.java
@@ -3,6 +3,7 @@ package model.product;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import connection.OracleConnection;
