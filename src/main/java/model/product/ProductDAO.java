09300bb7cb267d754a190dea7db7056035f0f31e
ammaryusufbakery <ammaryusufbakery@gmail.com>
2025-02-17 15:35:34 +0800

    Update ProductDAO.java

--- src/main/java/model/product/ProductDAO.java
+++ src/main/java/model/product/ProductDAO.java
@@ -15,22 +15,31 @@ PreparedStatement stmt2 = null;
 
     // Method to insert product
     public boolean insertFrame(Product product) {
-        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?)";
-        String query2 = "INSERT INTO frame (frameshape, framecolor, productid) VALUES (?, ?, productid_seq.currval)";
+        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?); SELECT SCOPE_IDENTITY() AS CURR_VAL";
+        String query2 = "INSERT INTO frame (frameshape, framecolor, productid) VALUES (?, ?, ?)";
+        int productId = 0;
         
         try {
         	con = OracleConnection.getConnection();
         	
             stmt = con.prepareStatement(query);
-            stmt2 = con.prepareStatement(query2);
              
             stmt.setDouble(1, product.getProductPrice());
             stmt.setString(2, product.getProductName());
             stmt.setString(3, product.getProductBrand());
+
+            ResultSet rs1 = stmt.executeQuery();
+
+            if (rs1.next()) {
+                productId = rs1.getInt("CURR_VAL");
+            }
+            
+            stmt2 = con.prepareStatement(query2);
+            
             stmt2.setString(1, product.getFrameShape());
             stmt2.setString(2, product.getFrameColor());
+            stmt2.setInt(3, productId);
 
-            stmt.executeUpdate();
             stmt2.executeUpdate();
             
             con.close();
@@ -43,22 +52,31 @@ PreparedStatement stmt2 = null;
     }
     
     public boolean insertLens(Product product) {
-        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?)";
-        String query2 = "INSERT INTO lens (lensprescription, lensastigmatism, productid) VALUES (?, ?, productid_seq.currval)";
+        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?); SELECT SCOPE_IDENTITY() AS CURR_VAL";
+        String query2 = "INSERT INTO lens (lensprescription, lensastigmatism, productid) VALUES (?, ?, ?)";
+        int productId = 0;
         
         try {
         	con = OracleConnection.getConnection();
         	
             stmt = con.prepareStatement(query);
-            stmt2 = con.prepareStatement(query2);
-             
+            
             stmt.setDouble(1, product.getProductPrice());
             stmt.setString(2, product.getProductName());
             stmt.setString(3, product.getProductBrand());
+
+            ResultSet rs1 = stmt.executeQuery();
+
+            if (rs1.next()) {
+                productId = rs1.getInt("CURR_VAL");
+            }
+            
+            stmt2 = con.prepareStatement(query2);
+            
             stmt2.setString(1, product.getLensPrescription());
             stmt2.setString(2, product.getLensAstigmatism());
-
-            stmt.executeUpdate();
+            stmt2.setInt(3, productId);
+            
             stmt2.executeUpdate();
             
             con.close();
