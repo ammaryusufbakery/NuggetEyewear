5bba169f274664a7139035bf9ff64322b38398be
ammaryusufbakery <ammaryusufbakery@gmail.com>
2025-07-01 23:50:20 +0800

    tukar ke generated keys

--- src/main/java/model/product/ProductDAO.java
+++ src/main/java/model/product/ProductDAO.java
@@ -16,24 +16,29 @@ PreparedStatement stmt2 = null;
 
     // Method to insert product
     public boolean insertFrame(Product product) {
-        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?); SELECT SCOPE_IDENTITY() AS CURR_VAL";
+        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?)";
         String query2 = "INSERT INTO frame (frameshape, framecolor, productid) VALUES (?, ?, ?)";
         int productId = 0;
         
         try {
         	con = OracleConnection.getConnection();
         	
-            stmt = con.prepareStatement(query);
+            stmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
              
             stmt.setDouble(1, product.getProductPrice());
             stmt.setString(2, product.getProductName());
             stmt.setString(3, product.getProductBrand());
 
-            ResultSet rs1 = stmt.executeQuery();
+            stmt.executeUpdate();
 
-            if (rs1.next()) {
+            /*if (rs1.next()) {
                 productId = rs1.getInt("CURR_VAL");
-            }
+            }*/
+
+            ResultSet rs2 = stmt.getGeneratedKeys();
+			while(rs2.next()) {
+				productId = rs2.getInt(1);
+			}
             
             stmt2 = con.prepareStatement(query2);
             
@@ -53,24 +58,29 @@ PreparedStatement stmt2 = null;
     }
     
     public boolean insertLens(Product product) {
-        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?); SELECT SCOPE_IDENTITY() AS CURR_VAL";
+        String query = "INSERT INTO product (productprice, productname, productbrand) VALUES (?, ?, ?)";
         String query2 = "INSERT INTO lens (lensprescription, lensastigmatism, productid) VALUES (?, ?, ?)";
         int productId = 0;
         
         try {
         	con = OracleConnection.getConnection();
         	
-            stmt = con.prepareStatement(query);
+            stmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             
             stmt.setDouble(1, product.getProductPrice());
             stmt.setString(2, product.getProductName());
             stmt.setString(3, product.getProductBrand());
 
-            ResultSet rs1 = stmt.executeQuery();
+            stmt.executeUpdate();
 
-            if (rs1.next()) {
+            /*if (rs1.next()) {
                 productId = rs1.getInt("CURR_VAL");
-            }
+            }*/
+
+            ResultSet rs2 = stmt.getGeneratedKeys();
+			while(rs2.next()) {
+				productId = rs2.getInt(1);
+			}
             
             stmt2 = con.prepareStatement(query2);
             
